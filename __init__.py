7cca37f56e579eb2b4df573518978554eaa3d67c
reber <reber0ask@protonmail.com>
2020-02-25 16:43:48 +0800

    add __init__.py

---
+++ __init__.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+'''
+@Author: reber
+@Mail: reber0ask@qq.com
+@Date: 2020-02-25 16:43:09
+@LastEditTime : 2020-02-25 16:43:21
+'''
+
+from .api.reports import AwvsReports
+from .api.vulns import AwvsVulns
+from .api.scans import AwvsScans
+from .api.targets import AwvsTargets
+from .api.dashboard import AwvsDashboard
